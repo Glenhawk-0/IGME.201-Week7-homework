ae2f2f46f3ce054e5d1c52fe1f8dd43be75c1fa5
Glenhawk-0 <LennonEhare@Gmail.com>
2024-10-06 12:52:18 -0400

    had to restart due to weird issue with name.
    so we start from here now

---
+++ grade_calculator/grade_calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace grade_calculator
+{
+    /*
+     * Lennon Hare
+     * IGME.201
+     * Due: Oct/13/2024
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //varibles
+            string myName = "Lennon";
+            int[] grades = new int[10] { 100, 92, 87, 200, -20, 52, 82, 75, 67, 88 };
+
+            //methods
+
+
+            //________________________________
+
+            //greetings
+            Console.WriteLine("welcome " + myName + ".\nhere are your grades.");
+
+
+        }
+    }
+}
+
