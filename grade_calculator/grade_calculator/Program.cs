4136e02ecebfe55de7d87a940b52d338bdd8ccef
Glenhawk-0 <LennonEhare@Gmail.com>
2024-10-06 14:00:21 -0400

    added... a bunch, its done though.
    having a weird issue where i cant quite run the code on my home desktop but i ran it elsewhere and it works fine

--- grade_calculator/grade_calculator/Program.cs
+++ grade_calculator/grade_calculator/Program.cs
@@ -7,56 +7,51 @@
      */
     internal class Program
     {
+     //global methods
+        static int checkGrade(int grade)
+        {//start check grade
+            if (grade >= 90)
+            {
+                if (grade == 100) { Console.WriteLine("WOW! A perfect score!"); }
+                Console.WriteLine("This grade is an A!");
+            }
+            else if (grade >= 80) { Console.WriteLine("This grade is a B."); }
+            else if (grade >= 70) { Console.WriteLine("This grade is a C."); }
+            else if (grade >= 65) { Console.WriteLine("This grade is a D."); }
+            else if (grade >= 0) { Console.WriteLine("This grade is a F."); }
+            else { Console.WriteLine("This is out of the range of 0-100. How did you even get this grade?"); }
+
+            return (grade);
+        }//end check grade
+
         static void Main(string[] args)
         {
-            //start copy
 
             //varibles
             string myName = "Lennon";
             int[] grades = new int[10] { 100, 92, 87, 200, -20, 52, 82, 75, 67, 88 };
             int average = -200;
-            //methods
-            static int checkGrade(int grade)
-            {
-                if (grade >= 90)
-                {
-                    if (grade == 100) { Console.WriteLine("WOW! A perfect score!"); }
-                    Console.WriteLine("This grade is an A!");
-                }
-                else if (grade >= 80) { Console.WriteLine("This grade is a B."); }
-                else if (grade >= 70) { Console.WriteLine("This grade is a C."); }
-                else if (grade >= 65) { Console.WriteLine("This grade is a D."); }
-                else if (grade >= 0 ) { Console.WriteLine("This grade is a F."); }
-                else { Console.WriteLine("This is out of the range of 0-100. How did you even get this grade?"); }
+            //________________________________
 
-                return (grade);
-
-
-            }//end checkGrade method
-                //________________________________
-
-                //greetings
-                Console.WriteLine("welcome " + myName + ".\nhere are your grades.");
+            //greetings
+            Console.WriteLine("welcome " + myName + ".\nhere are your grades.");
 
             //grades
             average = 0;
-            for (int i = 0; i < grades.Length; i++) 
+            for (int i = 0; i < grades.Length; i++)
             {
-                Console.WriteLine("Grade " + (i+1) +": " + grades[i]);
+                Console.WriteLine("\nGrade " + (i + 1) + ": " + grades[i]);
                 average += checkGrade(grades[i]);
-                
-
             }//end for loop
 
             //average
-            average = average / grades.Length;
-           Console.WriteLine("your average grade is " + average )
+            average = (average / grades.Length);
+            Console.WriteLine("\nyour average grade is " + average);
 
 
             //end it off
-
-           
-            //end copy
+            Console.WriteLine("\n-------------\n We have displayed all of " + myName + "'s grades");
+            
 
         }//end static void main
     }//end internal class program
